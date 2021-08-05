0758aede52ad8b04d5284f80c323a27c39e0d40f
salaza <gun159@outlook.com>
2021-08-05 14:19:03 +0700

    completed

--- script.js
+++ script.js
@@ -4,10 +4,45 @@ const btn_toggle = document.querySelector("#toggle");
 const length = document.getElementById('length')
 const color = document.querySelector('#color')
 
-// define more constants and variables here
+const p = document.getElementById("text");
+const origin_word = p.innerHTML;
+const origin_value = length.value;
+const btn_reset = document.getElementById("reset");
+const btn_search = document.getElementById("search");
+let check = 0;
 
 btn_toggle.onclick = () => {
-  // your code here
+  let id = 630610732 + parseInt(length.value);
+  if(check === 0){
+    author.innerHTML = "";
+    calculation.innerHTML = ""+id+"";
+    btn_toggle.innerText = "Show Author";
+    check = 1;
+  }
+  else{
+    calculation.innerHTML = "";
+    author.innerHTML = "630610732 TANHATAI KLUNGMAJAREON";
+    btn_toggle.innerText = "Show Calculation";
+    check = 0;
+  }
 }
 
 // more codes for Search and Reset buttons here
+btn_reset.onclick = () => {
+  p.innerHTML = origin_word;
+  length.value = origin_value;
+}
+
+btn_search.onclick = () => {
+  const text = p.innerText;
+  const words = text.split(" ");
+  p.innerHTML = "";
+  words.forEach( (word) => {
+    const span = document.createElement("span");
+    span.innerText = word+" ";
+    if(word.length >= length.value){
+      span.style.color = color.value;
+    }
+    p.appendChild(span);
+  })
+}
